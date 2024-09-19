c3cb15d266c083110259550786935f1c008b56a3
sptres <shawnpark2397@gmail.com>
2024-09-19 04:28:37 -0700

    initial backend setup

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
 
 // Config
 const __filename = fileURLToPath(import.meta.url);
@@ -34,10 +36,15 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+// Routes with files
+app.post('/auth/register', upload.single('picture'), register);
+// auth routes
+app.use('/auth', authRoutes);
+
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
