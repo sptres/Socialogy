bbdf91ace14388f997ae4a80cc549152d4961450
sptres <shawnpark2397@gmail.com>
2024-09-19 05:21:47 -0700

    backend set up complete

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
 
 // Config
 const __filename = fileURLToPath(import.meta.url);
@@ -38,8 +42,11 @@ const upload = multer({ storage: storage });
 
 // Routes with files
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 // auth routes
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
