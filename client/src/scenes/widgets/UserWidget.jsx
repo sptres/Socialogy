9f0ddabd2b3590a7b0374b0c11a3e296a897db84
sptres <shawnpark2397@gmail.com>
2024-09-20 18:25:43 -0700

    userimage

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,10 @@
+import {
+  ManageAccountOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from '@mui/icons-material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
+import UserImage from '../../components/UserImage';
+import FlexBetween from '../../components/FlexBetween';
+import WidgetWrapper from '../../components/WidgetWrapper';
